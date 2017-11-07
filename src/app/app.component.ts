674ca958fc52a0f000dee5672570e1c8e575e03c
jie1311 <tommy911113@gmail.com>
2017-11-07 15:27:35 +1100

    Initial Commit
    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Game} from './game';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Life Game';
+  game: Game;
+
+  constructor() {
+    this.game = new Game(100, 100);
+  }
+
+  reset(): void {
+    this.game.reset();
+  }
+
+  next100(): void {
+    for (let i = 0; i < 100; i++) {
+      this.game.next();
+    }
+  }
+
+  next(): void {
+    this.game.next();
+  }
 }
---
+++ src/app/game.ts
@@ -0,0 +1,161 @@
+export class Game {
+  private _height: number;
+  private _width: number;
+  private _status: any[];
+
+  constructor(height: number, width: number) {
+    this._height = height;
+    this._width = width;
+    this.init();
+  }
+
+  get height(): number {
+    return this._height;
+  }
+
+  set height(value: number) {
+    this._height = value;
+  }
+
+  get width(): number {
+    return this._width;
+  }
+
+  set width(value: number) {
+    this._width = value;
+  }
+
+  get status(): any[] {
+    return this._status;
+  }
+
+  set status(value: any[]) {
+    this._status = value;
+  }
+
+  private getRandomStatus(): number {
+    return Math.floor(Math.random() * 2);
+  }
+
+  init(): void {
+    this._status = [];
+    for (let i = 0; i < this._height; i++) {
+      this._status[i] = [];
+      for (let j = 0; j < this._width; j++) {
+        this._status[i][j] = this.getRandomStatus();
+      }
+    }
+  }
+
+  reset(): void {
+    for (let i = 0; i < this._height; i++) {
+      for (let j = 0; j < this._width; j++) {
+        this._status[i][j] = this.getRandomStatus();
+      }
+    }
+  }
+
+  next(): void {
+    const newStatus = [];
+    for (let i = 0; i < this._height; i++) {
+      newStatus[i] = [];
+      for (let j = 0; j < this._width; j++) {
+        if (i === 0 && j === 0) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[this._height - 1][this._width - 1],
+            this._status[this._height - 1][j],
+            this._status[this._height - 1][j + 1],
+            this._status[i][this._width - 1],
+            this._status[i][j + 1],
+            this._status[i + 1][this._width - 1],
+            this._status[i + 1][j],
+            this._status[i + 1][j + 1]);
+        } else if (i === 0 && j !== 0 && j !== this._width - 1) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[this._height - 1][j - 1],
+            this._status[this._height - 1][j],
+            this._status[this._height - 1][j + 1],
+            this._status[i][j - 1],
+            this._status[i][j + 1],
+            this._status[i + 1][j - 1],
+            this._status[i + 1][j],
+            this._status[i + 1][j + 1]);
+        } else if (i !== 0 && i !== this._height - 1 && j === 0) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[i - 1][this._width - 1],
+            this._status[i - 1][j],
+            this._status[i - 1][j + 1],
+            this._status[i][this._width - 1],
+            this._status[i][j + 1],
+            this._status[i + 1][this._width - 1],
+            this._status[i + 1][j],
+            this._status[i + 1][j + 1]);
+        } else if (i !== 0 && i !== this._height - 1 && j !== 0 && j !== this._width - 1) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[i - 1][j - 1],
+            this._status[i - 1][j],
+            this._status[i - 1][j + 1],
+            this._status[i][j - 1],
+            this._status[i][j + 1],
+            this._status[i + 1][j - 1],
+            this._status[i + 1][j],
+            this._status[i + 1][j + 1]);
+        } else if (i === this._height - 1 && j !== 0 && j !== this._width - 1) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[i - 1][j - 1],
+            this._status[i - 1][j],
+            this._status[i - 1][j + 1],
+            this._status[i][j - 1],
+            this._status[i][j + 1],
+            this._status[0][j - 1],
+            this._status[0][j],
+            this._status[0][j + 1]);
+        } else if (i !== 0 && i !== this._height - 1 && j === this._width - 1) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[i - 1][j - 1],
+            this._status[i - 1][j],
+            this._status[i - 1][0],
+            this._status[i][j - 1],
+            this._status[i][0],
+            this._status[i + 1][j - 1],
+            this._status[i + 1][j],
+            this._status[i + 1][0]);
+        } else if (i === this._height - 1 && j === this._width - 1) {
+          newStatus[i][j] = this.nextUnit(i, j,
+            this._status[i - 1][j - 1],
+            this._status[i - 1][j],
+            this._status[i - 1][0],
+            this._status[i][j - 1],
+            this._status[i][0],
+            this._status[0][j - 1],
+            this._status[0][j],
+            this._status[0][j + 1]);
+        }
+      }
+    }
+    this._status = newStatus;
+  }
+
+  private nextUnit(i: number, j: number,
+                   lu: number, uu: number, ru: number,
+                   ll: number, rr: number,
+                   ld: number, dd: number, rd: number): number {
+    if (this._status[i][j] === 1) {
+      if (lu + uu + ru + ll + rr + ld + dd + rd < 2) {
+        return 0;
+      } else if (lu + uu + ru + ll + rr + ld + dd + rd > 3) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else {
+      if (lu + uu + ru + ll + rr + ld + dd + rd === 3) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+}
+
